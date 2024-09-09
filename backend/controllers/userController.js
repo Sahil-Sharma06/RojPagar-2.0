513d119dc9f49af989f13ccd944ee87b70be9097
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-09-09 11:34:26 +0530

    Login Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,31 +1,26 @@
 import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     try {
         const { fullname, username, phone, password, confirmPassword, role, jobType, location } = req.body;
 
-        // Check if all fields are provided
         if (!fullname || !username || !phone || !password || !confirmPassword || !role || !jobType || !location) {
             return res.status(400).json({ message: "All fields are required!" });
         }
 
-        // Check if password matches confirmPassword
         if (password !== confirmPassword) {
             return res.status(400).json({ message: "Passwords do not match." });
         }
 
-        // Check if the user already exists
         const existingUser = await User.findOne({ username });
         if (existingUser) {
             return res.status(400).json({ message: "User already exists!" });
         }
 
-        // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Create the new user
         const newUser = await User.create({
             fullname,
             username,
@@ -36,13 +31,47 @@ export const register = async (req, res) => {
             location
         });
 
-        // Respond with success message
         return res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-        // Log error for debugging
         console.error('Error during registration:', error);
 
-        // Send error response
         return res.status(500).json({ message: "An error occurred during registration." });
     }
 };
+
+export const login = async (req,res)=>{
+    try {
+        const {username,password} = req.body;
+
+        if(!username || !password){
+            return res.status(400).json({message:"All fields are required."});
+        }
+
+        const user = await User.findOne({username});
+
+        if(!user){
+            return res.status(400).json({message:"User not found.",success:false})
+            
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordMatched){
+            return res.status(400).json({message:"Invalid Passwords!",success:false})
+        }
+
+        const tokenData = {
+           userId: user._id
+        }
+
+        const token = await jwt.sign(tokenData,process.env.JWT_SECRET_KEY,{expiresIn:"1d"});
+
+        return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000, httpOnly:true, sameSite:'strict'}).json({
+            _id:user._id,
+            username:user.username,
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
